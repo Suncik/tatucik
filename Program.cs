6fe1c15549132fabdc7f1f83ee1e262e112b50cd
Suncik <suncik@example.com>
2025-02-03 21:53:51 +0500

    Birinchi commit

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+class Program  
+{
+    static void Main()  
+    {
+        // //1-masala
+        // Console.Write("Sonni kiriting:>> ");
+        // int x = Convert.ToInt32(Console.ReadLine());
+
+        // if (x <= 1)
+        // {
+        //     Console.WriteLine("Tub emas");
+        //     return;
+        // }
+        // if (x == 2)
+        // {
+        //     Console.WriteLine("Tub");
+        //     return;
+        // }
+
+        // bool pc = true;
+        // for (int i = 2; i * i <= x; i++)
+        // {
+        //     if (x % i == 0)
+        //     {
+        //         pc = false;
+        //         break;
+        //     }
+        // }
+
+        // if (pc)
+        //     Console.WriteLine("Tub");
+        // else
+        //     Console.WriteLine("Tub emas");
+        //2-masala
+        // Console.Write("sonni kiriting:>>");
+        // int x=Convert.ToInt32(Console.ReadLine());
+        // for(int i=2; i<=10; i++){
+        //     if(x%i==0){
+        //         Console.Write($"{i} ");
+            //}
+    //        //4-masala
+    //    int[] harajatlar={12,13, 34};
+    //    int jami=harajatlar.Sum();
+    //    System.Console.WriteLine("umumiy harajat: "  +jami);
+    //5-masala
+    // Console.Write("sonni kiriting:>>");
+    // int x=Convert.ToInt32(Console.ReadLine());
+    // int temp=x;
+    //   int z=0;
+    // while(temp>0){
+    //     int y=temp%10;
+    //     temp/=10;
+      
+    //     z+=(int)Math.Pow(y, 3);
+
+    // }
+    // if(z==x){
+    //     System.Console.WriteLine("armstrong son");
+        
+    // }  else System.Console.WriteLine("armstrong emas");
+    // 6-masala
+    Console.Write("sonni kiriting:>>");
+    int x=Convert.ToInt32(Console.ReadLine());
+    int pc=0;
+    
+    for(int i=1; i<=x/2; i++){
+        
+        if(x%i==0){
+            pc++;
+            
+
+        }
+      
+    }
+      System.Console.WriteLine(pc);
+
+
+
+        }
+    }
+
